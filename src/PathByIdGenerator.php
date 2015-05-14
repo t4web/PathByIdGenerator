b142ac2b89544b73700812b1f19bacbf8ed7aa2a
maxgu <maxgu@t4web.com.ua>
2015-05-14 23:03:08 +0300

    add PathByIdGenerator

---
+++ src/PathByIdGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PathByIdGenerator;
+
+use InvalidArgumentException;
+
+class PathByIdGenerator
+{
+
+    /**
+     * generate folder path like /00/08/25/22/91
+     *
+     * @param int $objectId
+     * @param int $pathLength
+     * @param int $step
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return string
+     */
+    public function generatePath($objectId, $pathLength = 10, $step = 2)
+    {
+        if (empty($objectId) || !is_numeric($objectId) || (strlen($objectId) > $pathLength)) {
+            throw new InvalidArgumentException('Invalid argument $objectID');
+        }
+
+        // add zeros: "0008252291"
+        $stringId = str_repeat('0', ($pathLength - strlen($objectId))) . $objectId;
+
+        // prepare path "/00/08/25/22/91"
+        $path = '';
+        for ($i = 0; $i < $pathLength; $i += $step) {
+            $path .= '/' . substr($stringId, $i, $step);
+        }
+
+        return $path;
+    }
+}
